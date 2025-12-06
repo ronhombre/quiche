d9b5a145295abc306e3c84ed837e440e91943f4c
Ron Lauren Hombre <ronlauren@hombre.asia>
2025-12-06 20:43:05 +0800

    Test again 3

--- tokio-quiche/src/quic/connection/mod.rs
+++ tokio-quiche/src/quic/connection/mod.rs
@@ -495,7 +495,7 @@ where
             },
             Err(err) => {
                 let borrowed_err = Arc::new(err);
-                log::error!("QUIC handshake failed in IQC::start_with_result"; "error" => borrowed_err);
+                log::error!("QUIC handshake failed in IQC::start_with_result"; "error" => borrowed_err.clone());
                 Err(*borrowed_err) // Pass it upward
             }
         }
